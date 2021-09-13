dba7f8a06c8854a47280a2e8e2bf322b6d19aff7
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2021-09-13 16:10:10 -0300

    Upload da aula 01

---
+++ aula01/scripts.js
@@ -0,0 +1,6 @@
+var nota01 = parseFloat(prompt("Digite a 1ª nota do(a) aluno(a)"));
+var nota02 = parseFloat(prompt("Digite a 2ª nota do(a) aluno(a)"));
+var nota03 = parseFloat(prompt("Digite a 3ª nota do(a) aluno(a)"));
+var nota04 =parseFloat(prompt("Digite a 4ª nota do(a) aluno(a)"));
+var notaFinal = ((nota01 + nota02 + nota03 + nota04) / 4).toFixed(2);
+alert(`Média obtida: ${notaFinal}`)
